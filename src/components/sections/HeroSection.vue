1ed143b3f987e69162cc537331c8ba073a9f504a
nasr_galal <nasrbue@gmail.com>
2025-09-30 02:18:23 +0300

    fix(HeroSection): update video URL to new hero video
    The previous video URL was outdated and needed to be replaced with the new hero video for the package highlight.

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -4,7 +4,7 @@
     <div class="w-full">
       <!-- https://mhwjdkzpnhzmduolfgmy.supabase.co/storage/v1/object/public/videos/king/king.m3u8 -->
       <VideoPlayer
-        :videos="[{ id: 1, url: 'https://mhwjdkzpnhzmduolfgmy.supabase.co/storage/v1/object/public/videos/king/king.m3u8', title: 'package highlight' }]"
+        :videos="[{ id: 1, url: 'https://mhwjdkzpnhzmduolfgmy.supabase.co/storage/v1/object/public/videos/hero/inter/new-hero.m3u8', title: 'package highlight' }]"
         poster="https://mhwjdkzpnhzmduolfgmy.supabase.co/storage/v1/object/public/images/homepage/hero/king-tut-853x1280.webp"
         autoplay
       />
